fb2806ca59f94fc509e792945e16129e9f4403a2
Jack Fenton <mllax8@me.com>
2022-04-21 20:58:46 -0400

    Script that uses CustomPlayer and runs correctly

---
+++ doom-desire/custom_rl_gen8ou_agent.py
@@ -0,0 +1,174 @@
+# -*- coding: utf-8 -*-
+import asyncio
+
+import numpy as np
+import tensorflow as tf
+import wandb
+from gym.utils.env_checker import check_env
+from tabulate import tabulate
+
+from doom_desire.env_algorithm.dnq_training import example_dqn_structure
+from doom_desire.env_algorithm.dqn_evaluation import example_dqn_evaluation
+from doom_desire.example_teams.gen8ou import RandomTeamFromPool, team_1, team_2
+from doom_desire.helpers.simple_embedder import SimpleEmbedder
+from doom_desire.models.model_builder import ExampleSequentialModelBuilder
+from doom_desire.player.custom_player import CustomRLPlayer
+from doom_desire.player.rl_player_examples import ExampleRLPlayer
+from poke_env.player.baselines import MaxBasePowerPlayer, SimpleHeuristicsPlayer
+from poke_env.player.random_player import RandomPlayer
+
+from rl.agents.dqn import DQNAgent
+from rl.policy import LinearAnnealedPolicy, EpsGreedyQPolicy
+from rl.memory import SequentialMemory
+from tensorflow.keras.optimizers import Adam
+
+from poke_env.player.utils import background_evaluate_player, background_cross_evaluate
+
+# Defaults taken from reuniclusVGC
+# config_defaults = {
+#     'NB_TRAINING_STEPS': 10000,
+#     'NB_EVALUATION_EPISODES': 100,
+#     'first_layer_nodes': 500,
+#     'second_layer_nodes': 500,
+#     'third_layer_nodes': -1,
+#     'gamma': .99,
+#     'delta_clip': .9,
+#     'target_model_update': 10,
+#     'lr': .001,
+#     'memory_limit': 100000,
+#     'warmup_steps': 500,
+#     'activation': "relu",
+#     'policy': 'EpsGreedyQPolicy',
+#     'team': 'swampert',
+#     'opponent': 'max',
+#     'opponent_team': 'swampert'
+# }
+
+async def main():
+
+
+    config_defaults = {
+        'NB_TRAINING_STEPS': 10000,
+        'NB_EVALUATION_EPISODES': 100,
+        'first_layer_nodes': 128,   # 128       or 500
+        'second_layer_nodes': 64,   # 64        or 500
+        'third_layer_nodes': -1,
+        'gamma': 0.5,               # 0.5       or 0.99
+        'delta_clip': .01,          # .01       or 0.9
+        'target_model_update': 1,   # 1         or 10
+        'learning_rate': .00025,    # 0.00025   or 0.001
+        'memory_limit': 100000,
+        'warmup_steps': 1000,       # 1000      or 500
+        'activation': "elu",        # "elu"     or "relu"
+        'policy': 'EpsGreedyQPolicy',
+        'team': 'swampert',
+        'opponent': 'rand-max',
+        'opponent_team': 'swampert'
+    }
+
+    # Initialize a new wandb run; We can use os.environ['WANDB_MODE'] = 'dryrun' to not save wandb to cloud
+    wandb.init(config=config_defaults, entity="jfenton888", project="doom-desire-ai_DQN")
+    config = wandb.config
+
+    embedder = SimpleEmbedder()
+
+    # custom_builder = RandomTeamFromPool([team_1, team_2])
+    custom_builder = RandomTeamFromPool([team_1, team_2])
+
+    training_opponent = None
+    if config.opponent == 'rand':
+        training_opponent = RandomPlayer(battle_format="gen8ou", team=custom_builder)
+    elif config.opponent == 'max':
+        training_opponent = MaxBasePowerPlayer(battle_format="gen8ou", team=custom_builder)
+    elif config.opponent == 'heuristic':
+        training_opponent = SimpleHeuristicsPlayer(battle_format="gen8ou", team=custom_builder)
+    elif config.opponent == 'rand-max':
+        # Each battle is going to be against either a random player or a max base power player
+        training_opponent = [RandomPlayer(battle_format="gen8ou", team=custom_builder),
+                             MaxBasePowerPlayer(battle_format="gen8ou", team=custom_builder)]
+
+
+
+    # Create one environment for both training and evaluation
+    training_agent = CustomRLPlayer(battle_format="gen8ou",
+                                    config=config,
+                                    embedder=embedder,
+                                    team=custom_builder,
+                                    start_challenging=False)
+    # Train the agent against the opponent for num_steps
+    training_agent.train(training_opponent, num_steps=config.NB_TRAINING_STEPS)
+
+    # Create the first opponent to evaluate against
+    eval_opponent = RandomPlayer(battle_format="gen8ou", team=custom_builder)
+    # Reset the environment and set the new opponent to play against
+    training_agent.reset_env(opponent=eval_opponent, restart=True)
+
+    print("Results against random player:")
+    training_agent.evaluate_model(num_battles=config.NB_EVALUATION_EPISODES)
+    # dqn.test(eval_env, nb_episodes=100, verbose=False, visualize=False)
+    print(
+        f"DQN Evaluation: {training_agent.n_won_battles} victories out of {training_agent.n_finished_battles} episodes"
+    )
+
+    second_opponent = MaxBasePowerPlayer(battle_format="gen8ou", team=custom_builder)
+    training_agent.reset_env(opponent=second_opponent, restart=True)
+    print("Results against max base power player:")
+    training_agent.evaluate_model(num_battles=config.NB_EVALUATION_EPISODES)
+    print(
+        f"DQN Evaluation: {training_agent.n_won_battles} victories out of {training_agent.n_finished_battles} episodes"
+    )
+
+    third_opponent = SimpleHeuristicsPlayer(battle_format="gen8ou", team=custom_builder)
+    training_agent.reset_env(opponent=third_opponent, restart=True)
+    print("Results against simple heuristics player:")
+    training_agent.evaluate_model(num_battles=config.NB_EVALUATION_EPISODES)
+    print(
+        f"DQN Evaluation: {training_agent.n_won_battles} victories out of {training_agent.n_finished_battles} episodes"
+    )
+
+    training_agent.close()
+
+
+
+
+
+    # Evaluating the model
+
+    # eval_env.reset_env(restart=False)
+
+    # # Evaluate the player with included util method
+    # n_challenges = 250
+    # placement_battles = 40
+    # eval_task = background_evaluate_player(
+    #     eval_env.agent, n_challenges, placement_battles
+    # )
+    # dqn.test(eval_env, nb_episodes=n_challenges, verbose=False, visualize=False)
+    # print("Evaluation with included method:", eval_task.result())
+    # eval_env.reset_env(restart=False)
+    #
+    # # Cross evaluate the player with included util method
+    # n_challenges = 50
+    # players = [
+    #     eval_env.agent,
+    #     RandomPlayer(battle_format="gen8ou", team=custom_builder),
+    #     MaxBasePowerPlayer(battle_format="gen8ou", team=custom_builder),
+    #     SimpleHeuristicsPlayer(battle_format="gen8ou", team=custom_builder),
+    # ]
+    # cross_eval_task = background_cross_evaluate(players, n_challenges)
+    # dqn.test(
+    #     eval_env,
+    #     nb_episodes=n_challenges * (len(players) - 1),
+    #     verbose=False,
+    #     visualize=False,
+    # )
+    # cross_evaluation = cross_eval_task.result()
+    # table = [["-"] + [p.username for p in players]]
+    # for p_1, results in cross_evaluation.items():
+    #     table.append([p_1] + [cross_evaluation[p_1][p_2] for p_2 in results])
+    # print("Cross evaluation of DQN with baselines:")
+    # print(tabulate(table))
+
+
+if __name__ == "__main__":
+    asyncio.get_event_loop().run_until_complete(main())
+
